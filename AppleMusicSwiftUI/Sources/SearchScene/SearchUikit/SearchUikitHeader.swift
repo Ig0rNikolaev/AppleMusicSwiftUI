6d12673dafa5da6f2658e63a43b88ba42a763847
Igor Nikolaev <igor01131987@gmail.com>
2023-04-25 23:05:34 +0300

    Add uikit screen.

---
+++ AppleMusicSwiftUI/Sources/SearchScene/SearchUikit/SearchUikitHeader.swift
@@ -0,0 +1,50 @@
+//
+//  SearchUikitHeader.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Игорь Николаев on 25.04.2023.
+//
+
+import SwiftUI
+import UIKit
+
+class SearchUikitHeader: UICollectionReusableView {
+
+    static let identifier = "SearchUikitHeader"
+
+    //: MARK: - UI Elements
+
+    lazy var title: UILabel = {
+        let title = UILabel()
+        title.textColor = .black
+        title.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        return title
+    }()
+
+    // MARK: - Initializers
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+        setupHierarchy()
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    // MARK: - Setup
+
+    private func setupHierarchy() {
+        addSubview(title)
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            title.topAnchor.constraint(equalTo: topAnchor.self),
+            title.leftAnchor.constraint(equalTo: leftAnchor.self, constant: 16)
+        ])
+    }
+}
---
+++ AppleMusicSwiftUI/Sources/SearchScene/SearchUikit/UIkitSearchCategory.swift
@@ -0,0 +1,17 @@
+//
+//  UIkitSearchCategory.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Игорь Николаев on 25.04.2023.
+//
+
+import SwiftUI
+import UIKit
+
+struct UIKitSearchView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return UIkitSearchController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+}
